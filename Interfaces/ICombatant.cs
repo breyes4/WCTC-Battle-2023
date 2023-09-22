aec4ab214e12201bffd3ef86a07b8d1955a68faa
breyes4 <71798440+breyes4@users.noreply.github.com>
2023-09-21 23:45:16 -0500

    Update ICombatant.cs

--- Interfaces/ICombatant.cs
+++ Interfaces/ICombatant.cs
@@ -5,6 +5,7 @@
         public string Name { get; set; }
         public IArmor Armor { get; set; }
         public IWeapon Weapon { get; set; }
+        public IMagic Magic { get; set; }
 
         void Attack(ICombatant enemy);
     }
