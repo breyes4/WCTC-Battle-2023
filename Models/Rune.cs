5a903a6437877ad1f002f7d1c1c55dbc7af7d3e8
breyes4 <71798440+breyes4@users.noreply.github.com>
2023-09-21 23:52:23 -0500

    Update Rune.cs

--- Models/Rune.cs
+++ Models/Rune.cs
@@ -1,1 +1,21 @@
+﻿using Class05.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace Class05.Models
+{
+    public class Rune : IMagic
+    {
+        public string Name { get; set; }
+        public int Sorcery { get; set; }
+
+        public Rune(string name, int power)
+        {
+            Name = name;
+            Sorcery = sorcery;
+        }
+    }
+}
